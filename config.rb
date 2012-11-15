90b23e8fd3e4f4f337cb138e5e7484f1b063172f
Dragan Eror <draganeror@gmail.com>
2012-11-15 09:39:32 +0100

    Changed default values for environment and firesass variables.

--- config.rb
+++ config.rb
@@ -7,12 +7,12 @@
 ##
 
 # Change this to :production when ready to deploy the CSS to the live server.
-#environment = :development
-environment = :production
+environment = :development
+#environment = :production
 
 # In development, we can turn on the FireSass-compatible debug info.
-firesass = false
-#firesass = true
+#firesass = false
+firesass = true
 
 # Set this to the root of your project when deployed:
 http_path = "../"

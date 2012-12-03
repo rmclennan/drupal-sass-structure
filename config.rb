fe61558247a7c3b7b9d5fa66abc90eae1051d0a6
Dragan Eror <draganeror@gmail.com>
2012-12-03 18:57:46 +0100

    Update config.rb

--- config.rb
+++ config.rb
@@ -5,14 +5,25 @@
 ## If you'd like to learn more about Sass and Compass, see the sass/README.txt
 ## file for more information.
 ##
+## Run command line compile with:
+## 1. Force to compile compressed css for production.
+## 2. Force to compile expanded css with line numbers in comments useful for
+##    development.
+## 3. Force to compile expanded css with firesass info useful for easier
+##    debugging during development. (Require firesass Firefox addon)
+##
+##    1. ~$: compass compile -e production --force
+##    2. ~$: compass compile -e development --force
+##    3. ~$: compass compile --debug-info --force
+##
 
 # Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
+#environment = :development
 #environment = :production
 
 # In development, we can turn on the FireSass-compatible debug info.
 #firesass = false
-firesass = true
+#firesass = true
 
 # Set this to the root of your project when deployed:
 http_path = "../"
@@ -33,13 +44,14 @@ asset_cache_buster :none
 # You can select your preferred output style here (can be overridden via the
 # command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # Disable line comments if firesass plugin is enabled.
-line_comments = (environment == :development) ? (firesass == true) ? false : true : false;
+#line_comments = (environment == :development) ? (firesass == true) ? false : true : false;
+line_comments = (environment == :development) ? true : false;
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
@@ -49,4 +61,4 @@ line_comments = (environment == :development) ? (firesass == true) ? false : tru
 
 # Pass options to sass. For development, we turn on the FireSass-compatible
 # debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+#sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
